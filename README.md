f3ff562d8e8144150fc9f639928844caf182dd0f
Steffen Beyer <steffen@beyer.io>
2024-08-25 13:38:35 +0000

    Drop devshell flake

--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Nix DevShell for [NuttX](https://nuttx.apache.org) on ESP32
 
-based on https://github.com/numtide/devshell/
-and https://github.com/mirrexagon/nixpkgs-esp-dev/
+based on https://github.com/mirrexagon/nixpkgs-esp-dev/
 
 **Not ready yet!**
--- esp32/flake.nix
+++ esp32/flake.nix
@@ -1,19 +1,12 @@
 {
   description = "ESP32 toolchain for NuttX";
 
-  inputs.devshell.url = "github:numtide/devshell";
   inputs.esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
   inputs.flake-utils.url = "github:numtide/flake-utils";
-
-  # inputs.flake-compat = {
-  #   url = "github:edolstra/flake-compat";
-  #   flake = false;
-  # };
 
   outputs =
     {
       self,
-      devshell,
       esp-dev,
       flake-utils,
       nixpkgs,
@@ -26,22 +19,16 @@
             inherit system;
 
             overlays = [
-              devshell.overlays.default
               esp-dev.overlays.default
-              # (import "${esp-dev}/overlay.nix")
             ];
           };
         in
-        pkgs.devshell.mkShell {
-          imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
-          commands = [
-            { package = pkgs.esp-idf-esp32; }
-          ];
+        pkgs.mkShell {
           packages = with pkgs; [
             # attempts to download packages, we need to provide those beforehand
             # (callPackage ./toolchain.nix {})
 
-            # esp-idf-esp32
+            esp-idf-esp32
             # (esp-idf-esp32.override {
             #   rev = "cf7e743a9b2e5fd2520be4ad047c8584188d54da";
             #   sha256 = "sha256-tqWUTJlOWk4ayfQIxgiLkTrrTFU0ZXuh76xEZWKRZ/s=";
