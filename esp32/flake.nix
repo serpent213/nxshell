21e55ae0a077ce1680afb38cee1e6a71ae0ce2d3
Steffen Beyer <steffen@beyer.io>
2024-08-25 13:25:56 +0000

    Try with esp-dev flake instead of crosstool-NG

--- esp32/flake.nix
+++ esp32/flake.nix
@@ -2,18 +2,20 @@
   description = "ESP32 toolchain for NuttX";
 
   inputs.devshell.url = "github:numtide/devshell";
+  inputs.esp-dev.url = "github:mirrexagon/nixpkgs-esp-dev";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
-  inputs.flake-compat = {
-    url = "github:edolstra/flake-compat";
-    flake = false;
-  };
+  # inputs.flake-compat = {
+  #   url = "github:edolstra/flake-compat";
+  #   flake = false;
+  # };
 
   outputs =
     {
       self,
-      flake-utils,
       devshell,
+      esp-dev,
+      flake-utils,
       nixpkgs,
       ...
     }:
@@ -23,30 +25,43 @@
           pkgs = import nixpkgs {
             inherit system;
 
-            overlays = [ devshell.overlays.default ];
+            overlays = [
+              devshell.overlays.default
+              esp-dev.overlays.default
+              # (import "${esp-dev}/overlay.nix")
+            ];
           };
         in
         pkgs.devshell.mkShell {
           imports = [ (pkgs.devshell.importTOML ./devshell.toml) ];
+          commands = [
+            { package = pkgs.esp-idf-esp32; }
+          ];
           packages = with pkgs; [
             # attempts to download packages, we need to provide those beforehand
             # (callPackage ./toolchain.nix {})
 
+            # esp-idf-esp32
+            # (esp-idf-esp32.override {
+            #   rev = "cf7e743a9b2e5fd2520be4ad047c8584188d54da";
+            #   sha256 = "sha256-tqWUTJlOWk4ayfQIxgiLkTrrTFU0ZXuh76xEZWKRZ/s=";
+            # })
+
             # for interactive build
-            autoconf
-            automake
-            binutils
-            ncurses
-            gnused
-            gawk
-            gnum4
-            flex
-            yacc
-            texinfo
-            help2man
-            which
-            libtool
-            unzip
+            # autoconf
+            # automake
+            # binutils
+            # ncurses
+            # gnused
+            # gawk
+            # gnum4
+            # flex
+            # yacc
+            # texinfo
+            # help2man
+            # which
+            # libtool
+            # unzip
           ];
         };
     });
